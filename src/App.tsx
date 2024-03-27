9b3ec87eed460025c164c9b1f5149e1c6d244983
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-27 22:31:39 +0800

    update script

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CommandLineInterFace from "./components/CLI";
+import CLIDisplay from "./components/CLIDisplay";
 
 function App() {
   const [elements, setElements] = useState<JSX.Element[]>([]);
@@ -12,18 +13,25 @@ function App() {
       }
       if (cmd.toLowerCase() == 'new') {
         setElements([...elements,(<>
+        <CLIDisplay text={cmd}/>
         <h1 key={elements.length}>New Element</h1>
+        <br />
         </>)]);
         return;
       }
       if(cmd.split(' ')[0] == 'echo'){
         if(cmd.split(' ').length < 2){
-          setElements([...elements, (<p key={elements.length}>
+          setElements([...elements, (<>
+          <CLIDisplay text={cmd}/>
+          <p key={elements.length}>
             <span className="error">Error:</span> Command Doesnt Have Any Parameter Applied to It
-          </p>)]);
+          </p>
+          <br />
+          </>)]);
           return;
         };
         setElements([...elements,(<>
+        <CLIDisplay text={cmd}/>
         <p key={elements.length}>{cmd.split(' ').slice(1).join(' ')}</p>
         <br />
         </>)]);
@@ -31,10 +39,12 @@ function App() {
       }
       //Unknown Command Fallout
       setElements([...elements, (<>
+      <CLIDisplay text={cmd}/>
       <p key={elements.length}>
         <span className="error">Error:</span> <span className="high">'{c}'</span> is not recognized as an internal or external command.
       </p>
       <p>Use "<span className="high">help</span>" to see all commands</p>
+      <br />
       </>)]);
     };
 
---
+++ src/components/CLIDisplay.tsx
@@ -0,0 +1,14 @@
+import React,{ useState } from "react";
+import './CLI.css';
+
+function CLIDisplay(props:any){
+    return (
+        <div className="cli">
+            <h5>(<span>C:\root</span>)-[-]</h5>
+            <p>➜ <span className="display">{props.text}</span></p>
+            
+        </div>
+    )
+}
+
+export default CLIDisplay;
